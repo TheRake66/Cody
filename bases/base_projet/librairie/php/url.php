11467d5bbd82b0cc393671fb3b2f5f8a7ba294da
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2021-11-24 20:01:36 +0100

    ajout lib url php

--- bases/base_projet/librairie/php/url.php
+++ bases/base_projet/librairie/php/url.php
@@ -7,6 +7,29 @@ namespace Librairie;
 class Url {
 
 	/**
+	 * Contruit une url
+	 * 
+	 * @param string la route de destination
+	 * @param array les parametre avec nom et valeur
+	 * @param bool si on ajoute la memorisation du retour
+	 * @return string la nouvelle url
+	 */
+	public static function build($route, $param = [], $addback = false) {
+		$url = '?redirect=' . $route;
+
+		foreach ($param as $name => $value) {
+			$url .= '&' . $name . '=' . urlencode($value);
+		}
+
+		if ($addback) {
+			$url .= '&back=' . $_SERVER['REQUEST_URI'];
+		}
+
+		return $url;
+	}
+
+
+	/**
 	 * Remplace un parametre de l'url
 	 * 
 	 * @param string le nom du parametre
@@ -19,6 +42,49 @@ class Url {
 		return $_SERVER['PHP_SELF'] . '?' . http_build_query($query);
 	}
 	
+
+	/**
+	 * Supprime un paramettre de l'url
+	 * 
+	 * @param string nom du paramettre
+	 * @return string l'url sans le paramettre
+	 */
+	public static function removeGet($param) {
+		$query = $_GET;
+		unset($query[$param]);
+		return $_SERVER['PHP_SELF'] . '?' . http_build_query($query);
+	}
+	
+
+	/**
+	 * Defini une propriete HTML via la liste des paramettre GET, avec
+	 * une valeur par defaut si le paramettre n'existe pas
+	 * 
+	 * @param string nom du paramettre
+	 * @param string valeur par defaut
+	 * @param string propriete HTML
+	 * @return string la propriete avec la valeur
+	 */
+	public static function getValue($name, $default = '', $key = 'value') {
+		return $key . '="' . ($_GET[$name] ?? $default) . '"';
+
+	}
+	
+
+	/**
+	 * Defini une propriete HTML via la liste des paramettre POST, avec
+	 * une valeur par defaut si le paramettre n'existe pas
+	 * 
+	 * @param string nom du paramettre
+	 * @param string valeur par defaut
+	 * @param string propriete HTML
+	 * @return string la propriete avec la valeur
+	 */
+	public static function postValue($name, $default = '', $key = 'value') {
+		return $key . '="' . ($_POST[$name] ?? $default) . '"';
+
+	}
+
 }
 
 ?>
