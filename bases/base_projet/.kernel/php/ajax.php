96f146d2b2293fed8511db45875cd687f97ee6a6
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-03-14 10:30:49 +0100

    ajax log

--- bases/base_projet/.kernel/php/ajax.php
+++ bases/base_projet/.kernel/php/ajax.php
@@ -13,12 +13,7 @@ class Ajax {
 	 * @param object fonction anonyme a executee en cas de demande
 	 */
 	static function askGET($name, $fn) {
-        if (isset($_GET[$name])) {
-			$res = $fn();
-			ob_end_clean();
-			echo json_encode($res);
-            exit;
-        }
+		self::ajaxRun($_GET, 'get', $name, $fn);
 	}
 
 
@@ -29,8 +24,25 @@ class Ajax {
 	 * @param object fonction anonyme a executee en cas de demande
 	 */
 	static function askPOST($name, $fn) {
-        if (isset($_POST[$name])) {
+		self::ajaxRun($_POST, 'post', $name, $fn);
+	}
+
+
+	/**
+	 * Verifi, lance et log une fonction AJAX
+	 *
+	 * @param array la liste des variable a verifier
+	 * @param string le nom de la methode
+	 * @param string nom de la demande
+	 * @param object fonction anonyme a executee en cas de demande
+	 */
+	private static function ajaxRun($array, $method, $name, $fn) {
+        if (isset($array[$name])) {
+			Debug::log('Requête AJAX (méthode : "' . $method . '", composant : "' . debug_backtrace()[2]['class'] . '", fonction : "' .  $name . '", url : "' . $_SERVER['REQUEST_URI'] . '")...', Debug::LEVEL_PROGRESS);
+			Debug::log('Paramètres de la requête (ajax) : "' . print_r($array, true) . '".', Debug::LEVEL_INFO);
 			$res = $fn();
+			Debug::log('AJAX terminé', Debug::LEVEL_GOOD);
+			Debug::separator();
 			ob_end_clean();
 			echo json_encode($res);
             exit;
