d49130e2cfc60035b25e033590baa98d392bb625
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-04-22 16:30:57 +0200

    js cookies

--- bases/base_projet/.kernel/js/export.js
+++ bases/base_projet/.kernel/js/export.js
@@ -68,5 +68,53 @@ export default class Export {
         tab.document.write('<pre>' + content + '</pre>');
         tab.document.close();
     }
-    
+
+
+    /**
+     * Definie un cookie
+     * 
+     * @param {string} name le nom du cookie
+     * @param {string} value la valeur du cookie
+     * @param {Number} days la durée de vie du cookie en jours
+     * @param {string} path le chemin du cookie
+     * @returns void
+     */
+    static setCookie(name, value = null, days = null, path = null) {
+        let expires = null;
+        if (days) {
+            let date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = '; expires=' + date.toUTCString();
+        }
+        document.cookie = `${name}=${value || ''}${expires || ''}; path=${path || '/'}`;
+    }
+
+
+    /**
+     * Recupere un cookie
+     * 
+     * @param {string} name le nom du cookie
+     * @returns {string} la valeur du cookie
+     */
+    static getCookie(name) {
+        let c = document.cookie
+            .split(';')
+            .filter(cookie => cookie.trim().startsWith(`${name}=`));
+        if (c.length > 0) {
+            console.log(c[0].split('='));
+            return c[0].trim().split('=')[1];
+        }
+    }
+
+
+    /**
+     * Supprime un cookie
+     * 
+     * @param {string} name le nom du cookie
+     * @returns void
+     */
+    static deleteCookie(name) {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    }
+
 }
