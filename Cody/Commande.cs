425a26c82f640e30795f6b551148236edd7926b4
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-05-16 16:06:10 +0200

    bug unit

--- Cody/Commande.cs
+++ Cody/Commande.cs
@@ -842,8 +842,8 @@ vs                              Ouvre le projet dans Visual Studio Code.
                 string[] rep = Librairie.outputProcess("php", "-r \"" +
                     "set_error_handler(function() { }); " +
                     "register_shutdown_function(function() { }); " +
-                    "require_once(__DIR__ . '/.kernel/php/autoloader.php'); " +
-                    "Kernel\\Autoloader::register(); " +
+                    "require_once(__DIR__ . '/.kernel/php/io/autoloader.php'); " +
+                    "Kernel\\IO\\Autoloader::register(); " +
                     "require_once('" + obj.paths[0] + "'); " +
                     "new " + nspc + "();" +
                     "\"");
