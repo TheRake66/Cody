886e52379acab1a3a0308a2e8082174a346a3407
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-06-01 13:35:49 +0200

    event

--- bases/base_projet/.kernel/js/html/event.js
+++ bases/base_projet/.kernel/js/html/event.js
@@ -7,20 +7,20 @@
  * @license MIT License
  * @copyright © 2022 - Thibault BUSTOS (TheRake66)
  */
-export default class Event {
+ export default class Event {
 
     /**
      * Ajoute un evenement a un element HTML
      * 
-     * @param {HTMLElement} el l'element HTML
      * @param {string} event le nom de l'element
      * @param {function} callback la fonction à exécuter
+     * @param {HTMLElement} el l'element HTML
      * @param {boolean} capture si repercute l'element sur les element enfants
      * @param {boolean} once si l'element doit être écouté une seule fois
      * @param {boolean} passive si la fonction peut appeler la methode preventDefault()
      * @returns {void}
      */
-    static add(el, event, callback, capture = false, once = false, passive = false) {
+    static add(event, callback, el = document.body, capture = false, once = false, passive = false) {
         el.addEventListener(event, callback, {
             capture: capture,
             once: once,
