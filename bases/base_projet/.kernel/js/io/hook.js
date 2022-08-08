6ba2213c774ae89b7720d81ceb30496f60fb53d5
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-08-08 16:38:39 +0200

    hook lib js

---
+++ bases/base_projet/.kernel/js/io/hook.js
@@ -0,0 +1,40 @@
+
+import Dom from '../html/dom.js';
+import Builder from '../html/builder.js';
+import Finder from '../html/finder.js';
+
+
+
+/**
+ * Librairie gérant empêchant le changement de page.
+ * 
+ * @author Thibault Bustos (TheRake66)
+ * @version 1.0
+ * @category Framework source
+ * @license MIT License
+ * @copyright © 2022 - Thibault BUSTOS (TheRake66)
+ */
+export default class Hook {
+
+    /**
+	 * Ajoute l'écouteur de changement de page.
+     * 
+     * @returns {void}
+     */
+    static hook() {
+        window.onbeforeunload = function() {
+            return '';
+        }
+    }
+    
+
+    /**
+     * Supprime l'écouteur de changement de page.
+     * 
+     * @returns {void}
+     */
+    static unhook() {
+        window.onbeforeunload = {};
+    }
+    
+}
