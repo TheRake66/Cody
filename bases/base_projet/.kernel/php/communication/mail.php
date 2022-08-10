67d6559eb6ad6405cb3a8a2fbac96a129ea287ef
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-08-10 14:29:56 +0200

    mail html utf8

--- bases/base_projet/.kernel/php/communication/mail.php
+++ bases/base_projet/.kernel/php/communication/mail.php
@@ -60,7 +60,7 @@ abstract class Mail {
 		$headers = [ 'X-Mailer: PHP/' . phpversion() ];
 		if ($is_html) {
 			$headers[] = 'MIME-Version: 1.0';
-			$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+			$headers[] = 'Content-type: text/html; charset=utf-8';
 		} else {
 			$headers[] = 'Content-type: text/plain; charset=utf-8';
 		}
