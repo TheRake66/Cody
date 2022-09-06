81cd4664532c2f7eab17261b6532a5c5f7b4eaf0
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-09-06 14:12:47 +0200

    rest empty result

--- bases/base_projet/.kernel/php/communication/rest.php
+++ bases/base_projet/.kernel/php/communication/rest.php
@@ -163,14 +163,18 @@ abstract class Rest {
 	 * Génère un composant pour l'envoyer.
 	 * 
 	 * @param string $class La class du composant.
-	 * @param array $args Les paramètres du composant.
+	 * @param array|mixed $args La ou les paramètres du composant.
 	 * @return string Le composant généré.
 	 */
 	protected function generate($class, $args = []) {
 		if (Autoloader::typeof($class) === Autoloader::TYPE_CONTROLLER) {
 			return Stream::toogle(function() use ($class, $args) {
-				(new \ReflectionClass($class))
-					->newInstanceArgs($args);
+				if (is_array($args)) {
+					(new \ReflectionClass($class))
+						->newInstanceArgs($args);
+				} else {
+					new $class($args);
+				}
 			});
 		} else {
 			Error::trigger('La classe "' . $class . '" n\'est pas un composant !');
