fb13c32446af929ed98c355485765a45f9f052c5
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-09-07 10:59:34 +0200

    Update mount.js

--- bases/base_projet/.kernel/js/io/mount.js
+++ bases/base_projet/.kernel/js/io/mount.js
@@ -17,11 +17,27 @@ import Thread from '../io/thread.js';
 export default class Mount {
 
     /**
+     * @var {string} uuid L'identifiant unique du composant.
+     */
+    uuid = null;
+
+    /**
+     * @var {HTMLElement} $ L'élément du composant.
+     */
+    $ = null;
+
+    /**
+     * @var {Object} events La liste des événements enregistrés.
+     */
+    events = {};
+
+
+    /**
 	 * Monte un composant dans le script. Charge la balise principale du composant
      * ainsi que tous les éléments du composant.
 	 * 
      * @access public
-     * @param {string} uuid Le uuid du composant.
+     * @param {string} uuid L'identifiant unique du composant.
      * @return {void}
      */
     constructor(uuid) {
@@ -66,15 +82,16 @@ export default class Mount {
      * 
      * @param {string} event Le nom de l'événement.
      * @param {any} data Les données à envoyer à l'événement.
+     * @param {string} tag Un balise spécifique, seul les composants parent ayant cette balise seront déclencher.
      * @param {bool} cascade Si l'événement doit être déclenché que pour 
      * le premier composant parent ou tous jusqu'au premier composant de la page.
      * @return {void}
      */
-    emit(event = 'onrefresh', data = null, cascade = false) {
+    emit(event = 'refresh', data = null, tag = null, cascade = false) {
         let parent = this.$;
         do {
             parent = parent.parentElement.closest('component');
-            if (parent) {
+            if (parent && (tag === null || Finder.queryAll(`:scope > ${tag}` , parent).length > 0)) {
                 parent.dispatchEvent(new CustomEvent(event, {
                     detail: data
                 }));
@@ -88,18 +105,21 @@ export default class Mount {
      * 
      * @param {string} event Le nom de l'événement.
      * @param {any} data Les données à envoyer à l'événement.
+     * @param {string} tag Un balise spécifique, seul les composants enfant ayant cette balise seront déclencher.
      * @param {bool} cascade Si l'événement doit être déclenché que pour 
      * les premiers composants enfants ou tous jusqu'aux derniers composants de la page.
      * @return {void}
      */
-    pass(event = 'onrefresh', data = null, cascade = false) {
+    pass(event = 'refresh', data = null, tag = null, cascade = false) {
         let childrens = Finder.queryAll(cascade ?
             'component' : 
             'component:not(:scope > * component component)', this.$);
         childrens.forEach(child => {
-            child.dispatchEvent(new CustomEvent(event, {
-                detail: data
-            }));
+            if (tag === null || Finder.queryAll(tag, child).length > 0) {
+                child.dispatchEvent(new CustomEvent(event, {
+                    detail: data
+                }));
+            }
         });
     }
 
@@ -111,7 +131,8 @@ export default class Mount {
      * @param {string} event Le nom de l'événement.
      * @return {void}
      */
-    register(callback, event = 'onrefresh') {
+    register(callback, event = 'refresh') {
+        this.events[event] = callback;
         this.$.addEventListener(event, callback);
     }
 
@@ -122,8 +143,30 @@ export default class Mount {
      * @param {string} event Le nom de l'événement.
      * @return {void}
      */
-    unregister(event = 'onrefresh') {
-        this.$.removeEventListener(event);
+    unregister(event = 'refresh') {
+        this.$.removeEventListener(event, this.events[event]);
+        delete this.events[event];
+    }
+
+
+    /**
+     * Enregistre un événement, déclenche le même événement pour des composants enfant,
+     * attends la réponse, exécute la fonction, supprime l'événement.
+     * 
+     * @param {function} callback La fonction à exécuter lors de l'événement.
+     * @param {string} event Le nom de l'événement.
+     * @param {any} data Les données à envoyer à l'événement.
+     * @param {string} tag Un balise spécifique, seul les composants enfant ayant cette balise seront déclencher.
+     * @param {bool} cascade Si l'événement doit être déclenché que pour 
+     * les premiers composants enfants ou tous jusqu'aux derniers composants de la page.
+     * @return {void}
+     */
+    toogle(callback, event = 'get', data = null, tag = null, cascade = false) {
+        this.register(event => {
+            callback(event);
+            this.unregister(event);
+        }, event);
+        this.pass(event, data, tag, cascade);
     }
     
 }
