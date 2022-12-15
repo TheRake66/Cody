344fc5f7c5df42273901ee48eb7f715d1f156867
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2022-12-15 13:10:18 +0100

    js sound add onEnd event

--- bases/base_projet/.kernel/js/io/sound.js
+++ bases/base_projet/.kernel/js/io/sound.js
@@ -22,35 +22,37 @@ export default class Sound {
      * @param {string|Audio} track Le chemin du fichier audio.
      * @param {int} volume Le volume du son.
      * @param {boolean} loop Si le son doit être joué en boucle.
+     * @param {function} onSuccess La fonction à appeler si le son a été joué.
+     * @param {function} onError La fonction à appeler si le son n'a pas pu être joué.
+     * @param {function} onEnd La fonction à appeler quand le son est terminé.
      * @param {int} timeout Le temps avant chaque tentative de lecture du son.
      * @return {void}
      */
-    static async play(track, volume = 1, loop = false, onSuccess = null, onError = null, timeout = 500) {
+    static async play(track, volume = 1, loop = false, onSuccess = null, onError = null, onEnd = null, timeout = 500) {
         if (typeof track === 'string') {
             track = new Audio(track);
         }
         track.volume = volume;
-        if (loop) {
-            track.addEventListener('ended', function() {
-                this.currentTime = 0;
-                this.play();
-            }, false);
-        }
-        let played = false;
-        do {
-            track
-                .play()
-                .then(_ => {
-                    played = true;
-                    if (onSuccess) onSuccess();
-                })
-                .catch(e => {
-                    if (onError) onError(e);
-                });
-            if (!played) {
-                await Thread.sleep(timeout);
-            }
-        } while (!played)
+		track.addEventListener('ended', function() {
+			if (onEnd) onEnd();
+			if (loop) {
+				track.currentTime = 0;
+				track.play();
+			}
+		}, false);
+		do {
+			track
+				.play()
+				.then(_ => {
+					if (onSuccess) onSuccess();
+				})
+				.catch(e => {
+					if (onError) onError(e);
+				});
+			if (!track.played) {
+				await Thread.sleep(timeout);
+			}
+		} while (!track.played)
     }
 
 }
