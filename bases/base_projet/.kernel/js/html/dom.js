8e506463632080ec496d252e016ea4f9d77ebd06
BUSTOS THIBAULT <65202670+TheRake66@users.noreply.github.com>
2023-03-09 13:30:56 +0100

    dom prepend + position + inject

--- bases/base_projet/.kernel/js/html/dom.js
+++ bases/base_projet/.kernel/js/html/dom.js
@@ -13,7 +13,16 @@ import Builder from './builder.js';
  * @copyright © 2021-2023 - Thibault BUSTOS (TheRake66)
  */
 export default class Dom {
+
+    /**
+     * @type {string} Les positions possibles dans le DOM.
+     */
+	static POSITION_BEFORE_BEGIN = 'beforebegin';
+	static POSITION_AFTER_BEGIN  = 'afterbegin';
+	static POSITION_BEFORE_END   = 'beforeend';
+	static POSITION_AFTER_END    = 'afterend';
     
+
     /**
      * Vide le contenu d'un élément HTML.
      * 
@@ -44,7 +53,7 @@ export default class Dom {
      * @param {string} position L'endroit où insérer le contenu HTML.
      * @returns {void}
      */
-    static insert(html, el = document.body, position = 'beforeend') {
+    static insert(html, el = document.body, position = Dom.POSITION_BEFORE_END) {
         el.insertAdjacentHTML(position, html);
     }
 
@@ -83,6 +92,7 @@ export default class Dom {
      * 
      * @param {HTMLElement} el L'élément HTML à insérer.
      * @param {HTMLElement} sibling L'élément HTML après lequel insérer l'élément.
+     * @returns {void}
      */
     static after(el, sibling) {
         sibling.parentNode.insertBefore(el, sibling.nextSibling);
@@ -97,6 +107,18 @@ export default class Dom {
      */
     static before(el, sibling) {
         sibling.parentNode.insertBefore(el, sibling);
+    }
+
+
+    /**
+     * Insert un élément HTML dans le DOM en premier.
+     * 
+     * @param {HTMLElement} el L'élément HTML à insérer.
+     * @param {HTMLElement} parent Le parent de l'élément HTML.
+     * @returns {void}
+     */
+    static prepend(el, parent = document.body) {
+        parent.insertBefore(el, parent.firstChild);
     }
 
 
@@ -120,24 +142,49 @@ export default class Dom {
      * @param {string} position L'endroit où insérer le composant.
      * @returns {void}
      */
-    static inject(component, parent = document.body, position = 'beforeend') {
-        Dom.insert(component, parent, position);
+    static inject(component, parent = document.body, position = Dom.POSITION_BEFORE_END) {
+        let element = Builder.parse(component);
+        
+        switch (position) {
+            case 'beforebegin':
+                Dom.before(element, parent);
+                break;
 
+            case 'afterbegin':
+                Dom.prepend(element, parent);
+                break;
+
+            case 'beforeend':
+                Dom.append(element, parent);
+                break;
+
+            case 'afterend':
+                Dom.after(element, parent);
+                break;
+
+            default:
+                Dom.append(element, parent);
+                break;
+        }
+
+        let less = window.less;
         if (less !== undefined) {
             less.registerStylesheets();
             less.refresh();
         }
-
-        let element = position == 'beforeend' ? 
-            parent.lastElementChild : 
-            parent.firstElementChild;
-            
-        Finder.queryAll('script', element).forEach(script => {
-            let newScript = Builder.create('script', { 
-                type: 'module' 
-            }, script.innerHTML);
-            let container = script.parentNode;
-            container.replaceChild(newScript, script);
-        });
+        
+        let scripts = Finder.queryAll('script', element);
+        if (scripts) {
+            scripts.forEach(script => {
+                let html = script.innerHTML;
+                let type = script.type;
+                let container = script.parentNode;
+                let newScript = Builder.create('script', { 
+                    type: type
+                }, html);
+                container.replaceChild(newScript, script);
+            });
+        }
     }
+
 }
